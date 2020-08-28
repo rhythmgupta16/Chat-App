e0738d7ffd09a4729675bd637ec76e660cd83caa
Rhythm Gupta <rhythmgupta1234@gmail.com>
2020-08-28 23:47:32 +0530

    Basic Chat Functionality

---
+++ lib/login.dart
@@ -0,0 +1,414 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ChatApp/const.dart';
+import 'package:ChatApp/home.dart';
+import 'package:ChatApp/widget/loading.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences prefs;
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  TextEditingController emailInputController;
+  TextEditingController pwdInputController;
+
+  bool isLoading = false;
+  bool isLoggedIn = false;
+  User currentUser;
+
+  @override
+  void initState() {
+    emailInputController = new TextEditingController();
+    pwdInputController = new TextEditingController();
+    super.initState();
+    isSignedIn();
+  }
+
+  //Validate Email input
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Email format is invalid';
+    } else {
+      return null;
+    }
+  }
+
+  //Validate Password input
+  String pwdValidator(String value) {
+    if (value.length < 8) {
+      return 'Password must be longer than 8 characters';
+    } else {
+      return null;
+    }
+  }
+
+  void isSignedIn() async {
+    this.setState(() {
+      isLoading = true;
+    });
+
+    prefs = await SharedPreferences.getInstance();
+
+    isLoggedIn = await googleSignIn.isSignedIn();
+    if (isLoggedIn) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                HomeScreen(currentUserId: prefs.getString('id'))),
+      );
+    }
+
+    this.setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<Null> handleSignIn() async {
+    prefs = await SharedPreferences.getInstance();
+
+    this.setState(() {
+      isLoading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    User firebaseUser =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+
+    if (firebaseUser != null) {
+      // Check is already sign up
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection('users')
+          .where('id', isEqualTo: firebaseUser.uid)
+          .get();
+      final List<DocumentSnapshot> documents = result.docs;
+      if (documents.length == 0) {
+        // Update data to server if new user
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(firebaseUser.uid)
+            .set({
+          'nickname': firebaseUser.displayName,
+          'photoUrl': firebaseUser.photoURL,
+          'id': firebaseUser.uid,
+          'createdAt': DateTime.now().millisecondsSinceEpoch.toString(),
+          'chattingWith': null
+        });
+
+        // Write data to local
+        currentUser = firebaseUser;
+        await prefs.setString('id', currentUser.uid);
+        await prefs.setString('nickname', currentUser.displayName);
+        await prefs.setString('photoUrl', currentUser.photoURL);
+      } else {
+        // Write data to local
+        await prefs.setString('id', documents[0].data()['id']);
+        await prefs.setString('nickname', documents[0].data()['nickname']);
+        await prefs.setString('photoUrl', documents[0].data()['photoUrl']);
+        await prefs.setString('aboutMe', documents[0].data()['aboutMe']);
+      }
+      Fluttertoast.showToast(msg: "Sign in success");
+      this.setState(() {
+        isLoading = false;
+      });
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  HomeScreen(currentUserId: firebaseUser.uid)));
+    } else {
+      Fluttertoast.showToast(msg: "Sign in fail");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  Future<Null> handleEmailSignIn() async {
+    prefs = await SharedPreferences.getInstance();
+
+    this.setState(() {
+      isLoading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    User firebaseUser =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+
+    if (firebaseUser != null) {
+      // Check is already sign up
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection('users')
+          .where('id', isEqualTo: firebaseUser.uid)
+          .get();
+      final List<DocumentSnapshot> documents = result.docs;
+      if (documents.length == 0) {
+        // Update data to server if new user
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(firebaseUser.uid)
+            .set({
+          'nickname': firebaseUser.displayName,
+          'photoUrl': firebaseUser.photoURL,
+          'id': firebaseUser.uid,
+          'createdAt': DateTime.now().millisecondsSinceEpoch.toString(),
+          'chattingWith': null
+        });
+
+        // Write data to local
+        currentUser = firebaseUser;
+        await prefs.setString('id', currentUser.uid);
+        await prefs.setString('nickname', currentUser.displayName);
+        await prefs.setString('photoUrl', currentUser.photoURL);
+      } else {
+        // Write data to local
+        await prefs.setString('id', documents[0].data()['id']);
+        await prefs.setString('nickname', documents[0].data()['nickname']);
+        await prefs.setString('photoUrl', documents[0].data()['photoUrl']);
+        await prefs.setString('aboutMe', documents[0].data()['aboutMe']);
+      }
+      Fluttertoast.showToast(msg: "Sign in success");
+      this.setState(() {
+        isLoading = false;
+      });
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  HomeScreen(currentUserId: firebaseUser.uid)));
+    } else {
+      Fluttertoast.showToast(msg: "Sign in fail");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            // ClipPath is used to clip the child in a custom shape
+            clipper: BottomClipper(),
+            // here is the custom clipper for bottom cut shape
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(top: 40, bottom: 30),
+              margin: EdgeInsets.only(top: 30, left: 20, right: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(1, 2),
+                      spreadRadius: 1.0,
+                      blurRadius: 5.0)
+                ],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: <Widget>[
+                  // Email input
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.email), labelText: 'Email'),
+                      controller: emailInputController,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: emailValidator,
+                    ),
+                  ),
+                  // Password input
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: 'Password',
+                        suffixIcon: Icon(Icons.remove_red_eye),
+                      ),
+                      controller: pwdInputController,
+                      obscureText: true,
+                      validator: pwdValidator,
+                    ),
+                  ),
+                  // Forgot password
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 18.0),
+                      child: InkWell(
+                        // InkWell widget makes the widget clickable and provide call back for touch events
+                        onTap: () {
+                          Navigator.pushNamed(context, '/forgotPassword');
+                        },
+                        child: Text(
+                          'Forgot Password?',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                            color: Color(0xffFBB034),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Login using mobile number
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: InkWell(
+                      onTap: () {
+                        // Navigator.pushNamed(context, '/phone');
+                      },
+                      child: RichText(
+                        // RichText is used to styling a particular text span in a text by grouping them in one widget
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          text: 'Login using ',
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Mobile Number and OTP',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Submit button for login details
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () {
+                          // if (_loginFormKey.currentState.validate()) {
+                          //   FirebaseAuth.instance
+                          //       .signInWithEmailAndPassword(
+                          //           email: emailInputController.text,
+                          //           password: pwdInputController.text)
+                          //       .then((currentUser) => Firestore.instance
+                          //           .collection("users")
+                          //           .document(currentUser.uid)
+                          //           .get()
+                          //           .then((DocumentSnapshot result) =>
+                          //               Navigator.pushReplacement(
+                          //                   context,
+                          //                   MaterialPageRoute(
+                          //                       builder: (context) => HomePage(
+                          //                             title: "Home",
+                          //                             uid: currentUser.uid,
+                          //                           ))))
+                          //           .catchError((err) => print(err)))
+                          //       .catchError((err) => print(err));
+                          // }
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 20, top: 10),
+                          decoration: BoxDecoration(
+                              color: Color(0xffFBB034),
+                              borderRadius: BorderRadius.circular(30)),
+                          padding: EdgeInsets.all(8),
+                          child: Icon(
+                            Icons.navigate_next,
+                            size: 40,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          FlatButton(
+            onPressed: handleSignIn,
+            child: Text(
+              'SIGN IN WITH GOOGLE',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            color: Color(0xffdd4b39),
+            highlightColor: Color(0xffff7f7f),
+            splashColor: Colors.transparent,
+            textColor: Colors.white,
+            padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+          ),
+
+          // Loading
+          Positioned(
+            child: isLoading ? const Loading() : Container(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Custom Clipper Class
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    // Add Path lines to form slight cut
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height - 50);
+    return path;
+  }
+
+  // we don't need to render it again and again as UI renders
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'const.dart';
+import 'login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -8,59 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: themeColor,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: LoginScreen(title: 'CHAT DEMO'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
